3a99098480974e89be4ee99da1c29998df40ee2d
Christian Grabowski <christiang11754@gmail.com>
2016-07-27 23:45:59 -0400

    finish container setup

--- daemon/container.go
+++ daemon/container.go
@@ -1,15 +1,61 @@
 package daemon
 
 import (
+	"log"
+	"os"
 	"os/exec"
+	"syscall"
+
+	"github.com/cpg1111/spawn"
 )
+
+func spawndDir() string {
+	dir, dirErr := os.Getwd()
+	if dirErr != nil {
+		log.Fatal(dirErr)
+	}
+	return dir
+}
 
 type Container struct {
 	Proc
+	Name           string
+	Command        []string
+	PID            int
 	CGroupsPath    map[string]string
 	NamespacePaths map[string]string
+	conf           *config.Container
+}
+
+func NewContainer(confProc *config.Process) *Container {
+	return &Container{
+		Name:    confProc.Name,
+		Command: confProc.CMD,
+		conf:    confProc.Container,
+	}
 }
 
 func (c *Container) Start() (pid int, err error) {
+	dir := spawnDir()
+	cmdPath := fmt.Sprintf("%s/spawn-container")
+	cmd := exec.Command(cmdPath, c.Command...)
+	startErr := cmd.Start()
+	c.PID = cmd.Process.Pid
+	return c.PID, startErr
+}
 
+func (c *Container) Stop() error {
+	return syscall.Kill(c.PID, syscall.SIGTERM)
+}
+
+func (c *Container) Restart() (pid int, err error) {
+	err = c.Stop()
+	if err != nil {
+		return c.PID, err
+	}
+	return c.Start()
+}
+
+func (c *Container) Reload() error {
+	return syscall.Kill(c.PID, syscall.SIGHUP)
 }
