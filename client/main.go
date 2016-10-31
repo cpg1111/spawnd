8c65dd7dcb21803571e32c236faf3c26649ad4d2
Christian Grabowski <cpg1111@users.noreply.github.com>
2016-10-30 23:00:10 -0400

    Create client (#7)
    * add Vagrantfile for dev
    
    * WIP client
    
    * WIP bsd jail support
    
    * gitignore .vagrant
    
    * use proper file permissions (#6)

---
+++ client/main.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	cli "github.com/cpg1111/spawnd/client/pkg"
+	pb "github.com/cpg1111/spawnd/protobufs"
+)
+
+func main() {
+	payload := &pb.ProcessStateRequest
+	strArg := fmt.Sprint(os.Args[2:]...)
+	unmarshErr := json.Unmarshal(([]byte)(strArg), payload)
+	switch os.Args[1] {
+	case "start":
+		cli.Start(context.Background(), payload, grpc.WithInsecure())
+		break
+	case "stop":
+		cli.Stop(context.Background(), payload, grpc.WithInsecure())
+		break
+	case "reload":
+		cli.Reload(context.Background(), payload, grpc.WithInsecure())
+		break
+	case "restart":
+		cli.Restart(context.Background(), payload, grpc.WithInsecure())
+		break
+	}
+}
