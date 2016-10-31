80b35f60332dddd4443195b51847d358d883cec4
Christian Grabowski <cpg1111@users.noreply.github.com>
2016-10-30 22:59:57 -0400

    use proper file permissions (#6)

--- container/pkg/bsdjail/jailwrapper.h
+++ container/pkg/bsdjail/jailwrapper.h
@@ -1,12 +1,14 @@
+#include <unistd.h>
 #include <sys/param.h>
 #include <sys/jail.h>
+#include <sys/types.h>
 
 #ifndef JAILWRAPPER_H_
 #define JAILWRAPPER_H_
     typedef struct 
     {
-        jail bsd_jail;
-        char* user;
+        jail *bsd_jail;
+        uid_t user;
         int pid;
     } JailWrapper;
 
