6973b2471d69b88a0627e5aa3aa5eaf177cd7181
Christian Grabowski <christiang11754@gmail.com>
2016-12-13 11:47:13 -0500

    disable unsupported namespace

--- container/namespace/namespace.go
+++ container/namespace/namespace.go
@@ -16,8 +16,9 @@ func Setup(ns string) (uintptr, error) {
 		return syscall.CLONE_NEWNS, nil
 	case "net":
 		return syscall.CLONE_NEWNET, nil
-	case "cgroup":
-		return syscall.CLONE_NEWCGROUP, nil
+	// NOT SUPPORTED IN GO
+	//case "cgroup":
+	//	return syscall.CLONE_NEWCGROUP, nil
 	default:
 		return 0x0, fmt.Errorf("unsupported namespace %s", n.Type)
 	}
